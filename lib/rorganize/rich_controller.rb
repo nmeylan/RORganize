5d357074b3c2af37d7e3ca5d57e81bcb9e74b303
Nicolas Meylan <nmeylan@gmail.com>
2014-06-21 01:02:07 +0200

    Add module to enrich controllers

---
+++ lib/rorganize/rich_controller.rb
@@ -0,0 +1,45 @@
+# Author: Nicolas Meylan
+# Date: 21/06/2014
+# Encoding: UTF-8
+# File: rich_controller.rb
+# This allow controller to provide more actions as : bulk_edition or filters
+
+module Rorganize
+  module RichController
+    # Klazz : type of the filtered content, e.g : Issue
+    #Criterias : HASH criteria, e.g : {"subject"=>{"operator"=>"contains", "value"=>"test"}}
+    #Filter type : are content filtered? then value is filter (to filter content) or all (to display all result)
+    #Filter lists : Array of filters, e.g ["subject"] (used for the combobox)
+    #Commit : is a new filter is submit?
+    # session_json : serialized dom filter in json
+    # session_sql : sql filter
+    #Return : An array (size 2), first index -> sql filter, second index -> HASH criteria
+    def apply_filter(klazz, criteria, filter_type, filters_list, commit, session_json, session_sql)
+      if criteria
+        filter_params = criteria.clone
+        filter_params.delete_if { |_, filter| filter['operator'].eql?('all') }
+      else
+        filter_params = nil
+      end
+      filter = nil
+      if filter_type.eql?('filter') && criteria && filters_list && filters_list.any?
+        filter = klazz.conditions_string(criteria)
+      elsif commit
+        #filter SQL content
+        session_sql = nil
+        #filter DOM content
+        session_json = nil
+      end
+      #When page is reloading, user don't loose his filters
+      if filter_type && filter_type.eql?('filter')
+        session_json = filter_params
+      end
+      if filter
+        session_sql = filter
+      elsif !session_sql
+        session_sql = ''
+      end
+    end
+
+  end
+end
