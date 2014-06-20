fcfdacf8d642bc55b880b4241a7c81f6559abd4b
Nicolas Meylan <nmeylan@gmail.com>
2014-06-20 16:21:20 +0200

    Disable error display in production env.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,12 +6,12 @@ RORganize::Application.configure do
   config.cache_classes = true
   #
   #  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
   config.log_level = :error
   #  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
   #
   #  # Compress JavaScripts and CSS
   config.assets.compress = true
