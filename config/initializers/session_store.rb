2dca10ffd5dc21e3ac8fefd7f9e9eb139a3e1036
nmeylan <nmeylan@gmail.com>
2014-08-23 19:04:50 +0200

    Change session store strategy.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-RORganize::Application.config.session_store :cookie_store, key: '_ProjectManager_session'
+RORganize::Application.config.session_store :cache_store, key: '_Rorganize_sessions'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# ProjectManager::Application.config.session_store :active_record_store
+# Rorganize::Application.config.session_store :active_record_store
