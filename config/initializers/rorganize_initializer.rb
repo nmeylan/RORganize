141ed5db206ad0d0030b578eb0450b29d50f0d1c
nmeylan <nmeylan@gmail.com>
2014-09-06 22:21:28 +0200

    Add spring. Problem with spring is due to rubymine. Indeed, when we launch a rake task from rubymine, it run spring(RORganize/bin/spring) instead of run the rake task (RORganize/bin/rake).
    But when we run a rake task from command line it run (RORganize/bin/rake). Don't know why rubymine behave like this....

--- config/initializers/rorganize_initializer.rb
+++ config/initializers/rorganize_initializer.rb
@@ -2,12 +2,10 @@
 # Date: 9 févr. 2013
 # Encoding: UTF-8
 # File: rorganize_initalizer.rb
-
 unless $0.end_with?('rake')
   require 'rorganize'
-
   #If true user can register to application them self. If false, an administrator have to create an account for user.
-  RORganize::Application.config.rorganize_allow_user_self_registration = false
+  RORganize::Application.config.rorganize_allow_user_self_registration = true
   #If true unregister users can access to public project as Anonymous role. If false, user have to register to access to the application.
   RORganize::Application.config.rorganize_anonymous_access = true
 
