305c937e6646e6bb6be707142f8bfbd8b8d3c2b2
nmeylan <nmeylan@gmail.com>
2014-09-17 11:48:35 +0200

    Fix auto scroll.

--- app/decorators/application_collection_decorator.rb
+++ app/decorators/application_collection_decorator.rb
@@ -18,13 +18,15 @@ class ApplicationCollectionDecorator < Draper::CollectionDecorator
   # @param [Boolean] no_pagination : if false don't display pagination when there are too many results, else display pagination.
   # @param [String] no_data_text : text to display when there is no data to display.
   def display_collection(no_pagination = false, no_data_text = nil, no_scroll = false)
-    h.content_tag :div, {id: "#{h.controller_name}_content", class: "#{no_scroll ? '' : 'autoscroll'}"} do
+    h.content_tag :div, {id: "#{h.controller_name}_content"} do
       if object.to_a.any?
-        if block_given?
-          h.safe_concat yield
-        else
-          h.safe_concat h.list(self)
-        end
+        h.safe_concat h.content_tag :div, {class: "#{no_scroll ? '' : 'autoscroll'}"}, &Proc.new {
+          if block_given?
+            h.safe_concat yield
+          else
+            h.safe_concat h.list(self)
+          end
+        }
         h.safe_concat(h.paginate(object, h.session[h.controller_name.to_sym], pagination_path)) unless no_pagination || (object.to_a.size < 25 && h.session[h.controller_name.to_sym][:current_page].to_i < 2)
       else
         h.no_data(no_data_text)
