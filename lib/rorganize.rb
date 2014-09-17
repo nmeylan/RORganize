36273e01b7841a9a24720cc464d9ac590c9af7bb
nmeylan <nmeylan@gmail.com>
2014-09-17 08:17:34 +0200

    Change category icon.

--- lib/rorganize.rb
+++ lib/rorganize.rb
@@ -9,7 +9,7 @@ require 'rorganize/permission_manager'
 
 module Rorganize
   TIME_FORMAT = '%a. %-d %b. %I:%M %p.'
-  ACTION_ICON = {version_id: 'milestone', due_date: 'today', assigned_to_id: 'person', status_id: 'dashboard', category_id: 'package', done: 'pulse', tracker_id: 'issue-opened', admin: 'crown', email: 'mail'}
+  ACTION_ICON = {version_id: 'milestone', due_date: 'today', assigned_to_id: 'person', status_id: 'dashboard', category_id: 'tag', done: 'pulse', tracker_id: 'issue-opened', admin: 'crown', email: 'mail'}
   NON_MEMBER_ROLE = 'Non member'
 end
 
