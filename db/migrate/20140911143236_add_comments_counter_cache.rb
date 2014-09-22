ecd8e6658acf8fbc236a17275fdea2775b33a350
nmeylan <nmeylan@gmail.com>
2014-09-22 16:05:33 +0200

    Fix comments counter cache (update counters)

--- db/migrate/20140911143236_add_comments_counter_cache.rb
+++ db/migrate/20140911143236_add_comments_counter_cache.rb
@@ -1,17 +1,19 @@
 class AddCommentsCounterCache < ActiveRecord::Migration
   def up
-    add_column :issues, :comments_count, :integer, :default => 0
-    add_column :documents, :comments_count, :integer, :default => 0
+     add_column :issues, :comments_count, :integer, :default => 0
+     add_column :documents, :comments_count, :integer, :default => 0
 
-    sub = 'SELECT coalesce(count(c.id),0) FROM comments c WHERE i.id = c.commentable_id GROUP BY i.id'
-    sql = "UPDATE issues i set i.comments_count=(#{sub})"
-    ActiveRecord::Base.establish_connection(Rails.env.to_sym)
-    ActiveRecord::Base.connection.execute(sql)
-    ActiveRecord::Base.connection.execute('UPDATE issues i SET i.comments_count = 0 WHERE i.comments_count IS NULL')
-    sql = "UPDATE documents i set i.comments_count=(#{sub})"
-    ActiveRecord::Base.establish_connection(Rails.env.to_sym)
-    ActiveRecord::Base.connection.execute(sql)
-    ActiveRecord::Base.connection.execute('UPDATE documents i SET i.comments_count = 0 WHERE i.comments_count IS NULL')
+    Issue.transaction do
+      Issue.find_each do |i|
+        Issue.update_counters(i.id, comments_count: i.comments.count)
+      end
+    end
+    Document.transaction do
+      Document.find_each do |i|
+        Document.update_counters(i.id, comments_count: i.comments.count)
+      end
+    end
+
   end
 
   def down
