77e98e6aa6d40e7d5f1f075ecff4450b50ef8dab
nmeylan <nmeylan@gmail.com>
2014-10-02 07:29:57 +0200

    Add comment to paperclip patch.

--- lib/rorganize/patches/paperclip_patch.rb
+++ lib/rorganize/patches/paperclip_patch.rb
@@ -3,7 +3,13 @@
 # Encoding: UTF-8
 # File: paperclip_patch.rb
 
-
+# Reason of this patch :
+# The default behviour in paperclip is to call MIME class from ruby to detect the file's myme type.
+# But it lead to some detection errors. For example, if a .sql is upload the default ruby MIME class
+# detects the media_type as a application but when paperclip run the "file" unix command, it detects the
+# media type as a "text". So the @see "spoofed?" return false.
+# MimeMagic detect the same media type as the "file" unix command.
+# Now the @see "spoofed?" return true.
 module Rorganize
   module Patches
     module PaperclipPatch
