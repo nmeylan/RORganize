591e1ca904601ff32dbbf51b01b842e97d1b6d15
nmeylan <nmeylan@gmail.com>
2014-10-18 06:49:56 +0200

    Fix custom queries list.

--- app/helpers/queries_helper.rb
+++ app/helpers/queries_helper.rb
@@ -8,19 +8,20 @@ module QueriesHelper
   # @param [Array] collection of queries.
   def query_list(collection)
     content_tag :table, class: 'query list' do
-      safe_concat list_header(collection)
-      safe_concat(list_body(collection))
+      safe_concat query_list_header(collection)
+      safe_concat(query_list_body(collection))
     end
   end
 
-  def list_body(collection)
+  def query_list_body(collection)
     collection.collect do |query|
-      list_row(query)
+      query_list_row(query)
     end.join.html_safe
   end
 
-  def list_row(query)
+  def query_list_row(query)
     content_tag :tr, {class: 'odd-even query-tr', id: "#{query.id}"} do
+      list_td query.object_type, class: 'list-left object-type'
       list_td query.show_link, class: 'list-left name'
       list_td query.author, class: 'list-left author'
       list_td nil, {class: 'action list-right'}, &Proc.new {
@@ -30,8 +31,9 @@ module QueriesHelper
     end
   end
 
-  def list_header(collection)
+  def query_list_header(collection)
     content_tag :tr, class: 'header' do
+      list_th sortable('queries.object_type', 'type', collection.sortable_action), class: 'list-left'
       list_th sortable('queries.name', t(:field_name), collection.sortable_action), class: 'list-left'
       list_th sortable('users.name', t(:label_author), collection.sortable_action), class: 'list-left'
       list_th nil
