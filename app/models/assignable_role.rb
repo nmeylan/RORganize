3d526e5e20da80d8b928ef281e6f2d8aa4b1105b
nmeylan <nmeylan@gmail.com>
2014-10-18 15:45:34 +0200

    Remove useless model.

--- app/models/assignable_role.rb
+++
@@ -1,9 +0,0 @@
-# Author: Nicolas Meylan
-# Date: 18.10.14
-# Encoding: UTF-8
-# File: assignable_role.rb
-
-class AssignableRole < ActiveRecord::Base
-  belongs_to :role, class_name: 'Role'
-  belongs_to :assignable_by_role, class_name: 'Role'
-end
