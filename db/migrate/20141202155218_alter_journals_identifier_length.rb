62c8c8a4f8c5181e4dda7853f3da266c8435cb99
nmeylan <nmeylan@gmail.com>
2014-12-05 23:33:21 +0100

    Fix migration file, failed on teardown replace 'change' with 'up/down' combo.

--- db/migrate/20141202155218_alter_journals_identifier_length.rb
+++ db/migrate/20141202155218_alter_journals_identifier_length.rb
@@ -1,5 +1,9 @@
 class AlterJournalsIdentifierLength < ActiveRecord::Migration
-  def change
+  def up
+    change_column :journals, :journalizable_identifier, :string, length: 255
+  end
+
+  def down
     change_column :journals, :journalizable_identifier, :string, length: 255
   end
 end
