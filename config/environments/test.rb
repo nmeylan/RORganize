791da525f0bff7765be3ea256797415a5781331d
nmeylan <nmeylan@gmail.com>
2014-12-28 18:59:50 +0100

    Remove test env deprecation warning

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,7 +8,7 @@ RORganize::Application.configure do
   config.cache_classes = true
 
   # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.static_cache_control = "public, max-age=3600"
 
   # Log error messages when you accidentally call methods on nil
@@ -40,6 +40,10 @@ RORganize::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.active_support.test_order = :sorted
+
+  config.active_record.raise_in_transactional_callbacks = true
 
   config.eager_load = false
 end
