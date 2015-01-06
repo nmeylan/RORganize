3cdcd65bee9b71bebc6bb033e2d7db2ad0bead4e
nmeylan <nmeylan@gmail.com>
2015-01-06 20:59:48 +0100

    Add code coverage calculation. (even untouched files).

---
+++ test/code_coverage.rb
@@ -0,0 +1,75 @@
+# Author: Nicolas Meylan
+# Date: 06.01.15
+# Encoding: UTF-8
+# File: code_coverage.rb
+
+class CodeCoverage
+  GROUP_HASH = {
+      "Controller" => %w(app/controllers lib/rorganize/rich_controller),
+      "Decorators" => "app/decorators",
+      "Helpers" => %w(app/helpers lib/rorganize/helpers),
+      "Models" => %w(app/models lib/rorganize/models),
+      "QueryObjects" => "app/query_objects",
+      "ViewObjects" => "app/view_objects",
+      "Managers" => "lib/rorganize/managers",
+      "Filters" => "lib/rorganize/filters"
+  }
+  EXCLUDED_FILES = %w()
+
+  def self.start
+    SimpleCov.start 'rails' do
+      add_filter '/spec/'
+      add_filter '/db/'
+      add_filter '/lib/tasks/'
+      add_filter '/vendor/'
+      add_filter '/config/'
+      add_filter '/app/mailers/'
+
+
+      GROUP_HASH.each do |group_name, dir|
+        if dir.is_a?(Array)
+          g_name = group_name.eql?("Controller") ? "Controllers" : group_name
+          add_group(g_name, &->(source_file) { source_file.filename.include?(group_name.downcase) })
+        else
+          add_group group_name, dir
+        end
+      end
+
+      add_group 'Libraries', &->(source_file) { File.dirname(source_file.filename).end_with?('lib/rorganize') }
+    end
+
+    base_result = {}
+    GROUP_HASH.each_value do |dir|
+      if dir.is_a?(Array)
+        dir.each do |d|
+          base_result_for_source(base_result, d)
+        end
+      else
+        base_result_for_source(base_result, dir)
+      end
+    end
+    append_base_result_hash(Dir['lib/rorganize/*.rb'], base_result)
+  end
+
+  def self.base_result_for_source(base_result, dir)
+    all_files = Dir["#{dir}/**/*.rb"]
+    append_base_result_hash(all_files, base_result)
+  end
+
+  def self.append_base_result_hash(all_files, base_result)
+    all_files.each do |file|
+      absolute = File::expand_path(file)
+      lines = File.readlines(absolute, :encoding => 'UTF-8')
+      base_result[absolute] = lines.map do |l|
+        l.strip!
+        l.empty? || l =~ /^else$/ || l =~ /^end$/ || l[0] == '#' ? nil : 0
+      end
+    end
+
+    SimpleCov.at_exit do
+      merged = SimpleCov::Result.new(Coverage.result).original_result.merge_resultset(base_result)
+      result = SimpleCov::Result.new(merged)
+      result.format!
+    end
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,23 +2,9 @@ ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'simplecov'
-SimpleCov.start 'rails' do
-  add_filter '/spec/'
-  add_filter '/db/'
-  add_filter 'lib/tasks/'
-  add_filter '/vendor/'
-  add_filter '/config/'
-  add_filter '/app/mailers/'
+require 'code_coverage'
+CodeCoverage.start
 
-  add_group "Controllers", "app/controllers"
-  add_group "Decorators", "app/decorators"
-  add_group "Models", "app/models"
-  add_group "QueryObjects", "app/query_objects"
-  add_group "ViewObjects", "app/view_objects"
-
-  add_group "Managers", "lib/rorganize/managers"
-  add_group "Misc", "lib/rorganize"
-end
 
 
 class ActiveSupport::TestCase
@@ -33,3 +19,4 @@ class ActiveSupport::TestCase
   end
   # Add more helper methods to be used by all tests here...
 end
+
