4f12f7b2dc67c4e88173bd91e23a944412ca4dbd
nmeylan <nmeylan@gmail.com>
2015-01-06 19:01:10 +0100

    Add empty test files for controllers

---
+++ test/controllers/notifications_controller_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class NotificationsControllerTest < ActionController::TestCase
+  # Called before every test method runs. Can be used
+  # to set up fixture information.
+  def setup
+
+    # Do nothing
+  end
+
+  # Called after every test method runs. Can be used to tear
+  # down fixture information.
+
+  def teardown
+    # Do nothing
+  end
+
+end
