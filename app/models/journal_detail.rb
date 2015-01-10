a389eac102f19949edc7c13f7dcf0043a3a44c70
nmeylan <nmeylan@gmail.com>
2015-01-11 00:19:06 +0100

    Add delete all orphans feature for Journal detail. This method delete all Journal Detail for which journal doesn't exist for the given project.

--- app/models/journal_detail.rb
+++ app/models/journal_detail.rb
@@ -7,5 +7,10 @@ class JournalDetail < ActiveRecord::Base
   belongs_to :journal, class_name: 'Journal'
 
 
+  # @param [Numeric] project_id : delete all orphans journal details for this project id.
+  def self.delete_all_orphans(project_id)
+    journals_id = Journal.where(project_id: project_id).pluck('id')
+    JournalDetail.delete_all(['journal_id NOT IN (?)', journals_id.join(',')])
+  end
 end
 
--- test/models/journal_detail_test.rb
+++ test/models/journal_detail_test.rb
@@ -19,4 +19,29 @@ class JournalDetailTest < ActiveSupport::TestCase
     # Do nothing
   end
 
+  test 'it delete_all_orphans' do
+    orphan = JournalDetail.create(journal_id: 666, property: 'Assigned to', property_key: :assigned_to_id,
+                         old_value: nil, value: 'Nicolas Meylan')
+    orphan1 = JournalDetail.create(journal_id: 667, property: 'Assigned to', property_key: :assigned_to_id,
+                         old_value: nil, value: 'Nicolas Meylan')
+    orphan2 = JournalDetail.create(journal_id: 666, property: 'Assigned to', property_key: :assigned_to_id,
+                         old_value: nil, value: 'Nicolas Meylan')
+
+    journal = Journal.create(journalizable_type: 'Issue', journalizable_id: 666, action_type: 'created',
+                             project_id: 666, journalizable_identifier: 'aa', created_at: Time.new(2012, 10, 21))
+    non_orphan = JournalDetail.create(journal_id: journal.id, property: 'Assigned to', property_key: :assigned_to_id,
+                                      old_value: nil, value: 'Nicolas Meylan')
+    assert orphan.id
+    assert orphan1.id
+    assert orphan2.id
+    assert non_orphan.id
+
+    JournalDetail.delete_all_orphans(666)
+
+    assert_raise(ActiveRecord::RecordNotFound) { orphan.reload }
+    assert_raise(ActiveRecord::RecordNotFound) { orphan1.reload }
+    assert_raise(ActiveRecord::RecordNotFound) { orphan2.reload }
+    assert non_orphan.reload
+  end
+
 end
