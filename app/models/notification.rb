f6e25a81c2761fe010a76417293d151d53c22a37
nmeylan <nmeylan@gmail.com>
2015-01-10 19:12:54 +0100

    Cover Notification model. Add comment into Notification model.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -15,26 +15,52 @@ class Notification < ActiveRecord::Base
 
   # Delete all notification from the same notifiable and for the same user.
   def make_uniq
-    Notification.where.not(id: self.id).delete_all(user_id: self.user_id, notifiable_id: self.notifiable_id, notifiable_type: self.notifiable_type)
+    Notification.where.not(id: self.id).delete_all(user_id: self.user_id,
+                                                   notifiable_id: self.notifiable_id,
+                                                   notifiable_type: self.notifiable_type)
   end
 
-  def self.filter_notifications(filter, project, user)
-    notifications = Notification.includes(:project, :notifiable, :from).where(user_id: user.id).where(filter).order('notifications.created_at DESC')
+  # This method loads Notifications, prepare filters (per project and per recipient type) for a given user.
+  # @param [Hash|String] condition : a condition clause e.g : {recipient_type: 'watchers'}.
+  # if no restriction then give to the method '1=1'.
+  # @param [String] project_condition : a condition clause for the project e.g : {project_id: 1}
+  # if no restriction then give to the method '1=1'.
+  # @param [User] user : the user for who notifications should be loaded.
+  # @return [Array] an array of length 3.
+  # 'First' index : 'Array' loaded Notifications (an array of activerecords)
+  # 'Second' index : 'Hash' that contains filters with this structure :
+  #  {all: total_notification_count, participants: total_notification_for_participant_type, watchers: same but for watchers}
+  # 'Third' index : 'Hash' with following structure
+  # { project_slug => { count: number_notification, id: project_id } }
+  def self.filter_notifications(condition, project_condition, user)
+    notifications = Notification.includes(:project, :notifiable, :from)
+                        .where(user_id: user.id)
+                        .where(condition)
+                        .order('notifications.created_at DESC')
 
     count_participating, count_watching = count_notification_by_recipient_type(user)
-    projects = {}
-    count_notifications_by_projects(notifications, projects)
+    projects = count_notifications_by_projects(notifications)
     filters = {all: count_participating + count_watching, participants: count_participating, watchers: count_watching}
 
-    return notifications.where(project), filters, projects
+    return notifications.where(project_condition), filters, projects
   end
 
-  def self.count_notifications_by_projects(notifications, projects)
+  # @param [Array] notifications : an array containing notifications.
+  # @return [Hash] a Hash with the following structure :
+  # { project_slug => { count: number_notification, id: project_id } }
+  def self.count_notifications_by_projects(notifications)
+    projects = {}
     notifications.map(&:project).uniq.each do |notification_project|
-      projects[notification_project.slug] = {count: notifications.to_a.count { |notif| notif.project_id.eql?(notification_project.id) }, id: notification_project.id}
+      projects[notification_project.slug] = {count: notifications.to_a.count { |notif| notif.project_id.eql?(notification_project.id) },
+                                             id: notification_project.id}
     end
+    projects
   end
 
+  # @param [User] user : the user for who we should count notifications.
+  # @return [Array] an array of length 2.
+  # First index : count for participating notifications.
+  # Second index : count for watching notifications.
   def self.count_notification_by_recipient_type(user)
     count_participating = Notification.where(user_id: user.id, recipient_type: 'participants').count('id')
     count_watching = Notification.where(user_id: user.id, recipient_type: 'watchers').count('id')
---
+++ test/models/notification_test.rb
@@ -0,0 +1,103 @@
+# Author: Nicolas Meylan
+# Date: 10.01.15
+# Encoding: UTF-8
+# File: notification_test.rb
+require 'test_helper'
+
+class NotificationTest < ActiveSupport::TestCase
+
+  # Called before every test method runs. Can be used
+  # to set up fixture information.
+  def setup
+    @user = users(:users_001)
+    @user1 = users(:users_002)
+    @project = Project.create(name: 'Rorganize test')
+    @project1 = Project.create(name: 'Rorganize test bis')
+  end
+
+  # Called after every test method runs. Can be used to tear
+  # down fixture information.
+
+  def teardown
+    # Do nothing
+  end
+
+
+  test "it should be uniq for the a same item" do
+    issue = Issue.create(tracker_id: 1, subject: 'Bug1', status_id: 1, project_id: @project.id)
+    journal = Journal.find_by_journalizable_id_and_journalizable_type_and_action_type(issue.id, 'Issue', 'created')
+    notification = Notification.create(user_id: @user.id, notifiable_id: issue.id,
+                                       notifiable_type: 'Issue', project_id: 1, from_id: @user1.id,
+                                       trigger_type: 'Journal',
+                                       trigger_id: journal.id,
+                                       recipient_type: 'participants')
+
+    assert 1, Notification.where(user_id: @user.id,
+                                 notifiable_id: issue.id,
+                                 notifiable_type: 'Issue').count
+
+
+    notification = Notification.create(user_id: @user.id, notifiable_id: issue.id,
+                                       notifiable_type: 'Issue', project_id: 1, from_id: @user1.id,
+                                       trigger_type: 'Journal',
+                                       trigger_id: journal.id,
+                                       recipient_type: 'participants')
+    assert 1, Notification.where(user_id: @user.id,
+                                 notifiable_id: issue.id,
+                                 notifiable_type: 'Issue').count
+  end
+
+  test 'it count notification per project' do
+    notifications = create_notifications
+    expectation = {
+        @project.slug => {count: 1, id: @project.id},
+        @project1.slug => {count: 3, id: @project1.id}
+    }
+
+    assert_equal expectation, Notification.count_notifications_by_projects(notifications)
+  end
+
+  test 'it should count notification per recipient type' do
+    create_notifications
+
+    assert_equal 3, Notification.count_notification_by_recipient_type(@user).first # participants
+    assert_equal 1, Notification.count_notification_by_recipient_type(@user)[1] # watchers
+  end
+
+  test 'it should load notification and prepare filter for a given user' do
+    notifications = create_notifications
+    expectation = [
+        notifications,
+        {all: 4, participants: 3, watchers: 1},
+        {@project.slug => {count: 1, id: @project.id}, @project1.slug => {count: 3, id: @project1.id}}]
+    result_notifications, filters, projects_filter = Notification.filter_notifications('1=1', '1=1', @user)
+    assert_equal expectation[0], result_notifications
+    assert_equal expectation[1], filters
+    assert_equal expectation[2], projects_filter
+  end
+
+  private
+  def create_notifications
+    issues = []
+    issues << Issue.create(tracker_id: 1, subject: 'Bug1', status_id: 1, project_id: @project1.id)
+    issues << Issue.create(tracker_id: 1, subject: 'Bug1', status_id: 1, project_id: @project1.id)
+    issues << Issue.create(tracker_id: 1, subject: 'Bug1', status_id: 1, project_id: @project1.id)
+    notifications = []
+    issues.each do |issue|
+      journal = Journal.find_by_journalizable_id_and_journalizable_type_and_action_type(issue.id, 'Issue', 'created')
+      notifications << Notification.create(user_id: @user.id, notifiable_id: issue.id,
+                                           notifiable_type: 'Issue', project_id: issue.project_id, from_id: @user1.id,
+                                           trigger_type: 'Journal',
+                                           trigger_id: journal.id,
+                                           recipient_type: 'participants')
+    end
+
+    issue = Issue.create(tracker_id: 1, subject: 'Bug1', status_id: 1, project_id: @project.id)
+    journal = Journal.find_by_journalizable_id_and_journalizable_type_and_action_type(issue.id, 'Issue', 'created')
+    notifications << Notification.create(user_id: @user.id, notifiable_id: issue.id,
+                                         notifiable_type: 'Issue', project_id: issue.project_id, from_id: @user1.id,
+                                         trigger_type: 'Journal',
+                                         trigger_id: journal.id,
+                                         recipient_type: 'watchers')
+  end
+end
