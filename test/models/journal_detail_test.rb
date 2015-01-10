019908db73ddacb01518db80eac0d55a3613edea
nmeylan <nmeylan@gmail.com>
2015-01-10 16:04:47 +0100

    Add empty test file for journal detail model.

---
+++ test/models/journal_detail_test.rb
@@ -0,0 +1,22 @@
+# Author: Nicolas Meylan
+# Date: 10.01.15
+# Encoding: UTF-8
+# File: journal_detail_test.rb
+require 'test_helper'
+
+class JournalDetailTest < ActiveSupport::TestCase
+
+  # Called before every test method runs. Can be used
+  # to set up fixture information.
+  def setup
+    # Do nothing
+  end
+
+  # Called after every test method runs. Can be used to tear
+  # down fixture information.
+
+  def teardown
+    # Do nothing
+  end
+
+end
