9c26b2c2de739107192acd40d9dba7812dbc4cb5
nmeylan <nmeylan@gmail.com>
2015-01-12 00:18:35 +0100

    Git reformat test helper code.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -30,14 +30,14 @@ class ActiveSupport::TestCase
 end
 
 class Object
-  def self.stub_any_instance name, val_or_callable, &block
+  def self.stub_any_instance(name, val_or_callable, &block)
     new_name = "__minitest_any_instance_stub__#{name}"
 
     class_eval do
       alias_method new_name, name
 
       define_method(name) do |*args|
-        if val_or_callable.respond_to? :call then
+        if val_or_callable.respond_to?(:call)
           val_or_callable.call(*args)
         else
           val_or_callable
@@ -48,9 +48,9 @@ class Object
     yield
   ensure
     class_eval do
-      undef_method name
-      alias_method name, new_name
-      undef_method new_name
+      undef_method(name)
+      alias_method(name, new_name)
+      undef_method(new_name)
     end
   end
 end
