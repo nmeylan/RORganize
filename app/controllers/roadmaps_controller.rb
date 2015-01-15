3e22ea670bcfdce7ca88d762f62a7a01ed5872c8
nmeylan <nmeylan@gmail.com>
2015-01-15 15:23:43 +0100

    Remove dead code.

--- app/controllers/roadmaps_controller.rb
+++ app/controllers/roadmaps_controller.rb
@@ -28,24 +28,10 @@ class RoadmapsController < ApplicationController
     @version_decorator = Version.eager_load(issues: [:status, :tracker]).find_by_id(params[:id])
     if @version_decorator
       @version_decorator = @version_decorator.decorate
-
     else
       render_404
     end
   end
-
-
-  def calendar
-    @versions = @project_decorator.versions.order(:position)
-    calendar = Version.define_calendar(@versions, params[:date])
-    @versions_hash = calendar[:versions_hash]
-    @date = calendar[:date]
-    respond_to do |format|
-      format.html
-      format.js { respond_to_js }
-    end
-  end
-
 
   private
   def find_project
