24f4e18bc3d32b5661dee3a20fabe4ebc4c85957
nmeylan <nmeylan@gmail.com>
2015-01-16 16:09:47 +0100

    Cover commentable behavior.

---
+++ test/models/behaviors/commentable/document_commentable_test.rb
@@ -0,0 +1,64 @@
+# Author: Nicolas Meylan
+# Date: 16.01.15 15:55
+# Encoding: UTF-8
+# File: issue_commentable_test.rb
+require 'test_helper'
+
+class IssueCommentableTest < ActiveSupport::TestCase
+
+  # Called before every test method runs. Can be used
+  # to set up fixture information.
+  def setup
+    # Do nothing
+  end
+
+  # Called after every test method runs. Can be used to tear
+  # down fixture information.
+
+  def teardown
+    # Do nothing
+  end
+
+  test "it has many comments and should be destroy when commentable is destroyed" do
+    document = Document.create(name: 'Issue creation', description: '')
+    comment = Comment.new(content: 'this a comment', user_id: User.current.id, project_id: 1, commentable_id: document.id, commentable_type: 'Document')
+    comment1 = Comment.new(content: 'this a comment', user_id: User.current.id, project_id: 1, commentable_id: document.id, commentable_type: 'Document')
+    comment2 = Comment.new(content: 'this a comment', user_id: User.current.id, project_id: 1, commentable_id: document.id, commentable_type: 'Document')
+    assert comment.save
+    assert comment1.save
+    assert comment2.save
+    assert_equal [comment, comment1, comment2], document.comments
+
+    document.destroy
+    assert_raise(ActiveRecord::RecordNotFound) {comment.reload}
+    assert_raise(ActiveRecord::RecordNotFound) {comment1.reload}
+    assert_raise(ActiveRecord::RecordNotFound) {comment2.reload}
+  end
+
+  test "does it has been commented" do
+    document = Document.create(name: 'Issue creation', description: '')
+    assert_not document.commented?
+
+    comment = Comment.new(content: 'this a comment', user_id: User.current.id, project_id: 1, commentable_id: document.id, commentable_type: 'Document')
+    assert comment.save
+
+    document.reload
+    assert document.commented?
+  end
+
+  test "it as a method to bulk delete all comments for a given commentable items" do
+    document = Document.create(name: 'Issue creation', description: '')
+    comment = Comment.new(content: 'this a comment', user_id: User.current.id, project_id: 1, commentable_id: document.id, commentable_type: 'Document')
+    comment1 = Comment.new(content: 'this a comment', user_id: User.current.id, project_id: 1, commentable_id: document.id, commentable_type: 'Document')
+    comment2 = Comment.new(content: 'this a comment', user_id: User.current.id, project_id: 1, commentable_id: 666, commentable_type: 'Document')
+    assert comment.save
+    assert comment1.save
+    assert comment2.save
+
+    Rorganize::Models::Commentable::bulk_delete_dependent([document.id], 'Document')
+    assert document.reload
+    assert_raise(ActiveRecord::RecordNotFound) {comment.reload}
+    assert_raise(ActiveRecord::RecordNotFound) {comment1.reload}
+    assert comment2.reload
+  end
+end
---
+++ test/models/behaviors/commentable/issue_commentable_test.rb
@@ -0,0 +1,62 @@
+# Author: Nicolas Meylan
+# Date: 16.01.15 15:55
+# Encoding: UTF-8
+# File: issue_commentable_test.rb
+require 'test_helper'
+
+class IssueCommentableTest < ActiveSupport::TestCase
+
+  # Called before every test method runs. Can be used
+  # to set up fixture information.
+  def setup
+    # Do nothing
+  end
+
+  # Called after every test method runs. Can be used to tear
+  # down fixture information.
+
+  def teardown
+    # Do nothing
+  end
+
+  test "it has many comments and should be destroy when commentable is destroyed" do
+    issue = Issue.create(tracker_id: 1, subject: 'Issue creation', description: '', status_id: '1', done: 0, project_id: 1, due_date: '2012-12-31')
+    comment = Comment.create(content: 'this a comment', user_id: User.current.id, project_id: 1, commentable_id: issue.id, commentable_type: 'Issue')
+    comment1 = Comment.create(content: 'this a comment', user_id: User.current.id, project_id: 1, commentable_id: issue.id, commentable_type: 'Issue')
+    comment2 = Comment.create(content: 'this a comment', user_id: User.current.id, project_id: 1, commentable_id: issue.id, commentable_type: 'Issue')
+
+    assert_equal [comment, comment1, comment2], issue.comments
+
+    issue.destroy
+    assert_raise(ActiveRecord::RecordNotFound) {comment.reload}
+    assert_raise(ActiveRecord::RecordNotFound) {comment1.reload}
+    assert_raise(ActiveRecord::RecordNotFound) {comment2.reload}
+  end
+
+  test "does it has been commented" do
+    issue = Issue.create(tracker_id: 1, subject: 'Issue creation', description: '', status_id: '1', done: 0, project_id: 1, due_date: '2012-12-31')
+    assert_not issue.commented?
+
+    comment = Comment.new(content: 'this a comment', user_id: User.current.id, project_id: 1, commentable_id: issue.id, commentable_type: 'Issue')
+    assert comment.save
+
+    issue.reload
+    assert issue.commented?
+  end
+
+  test "it as a method to bulk delete all comments for a given commentable items" do
+    issue = Issue.create(tracker_id: 1, subject: 'Issue creation', description: '', status_id: '1', done: 0, project_id: 1, due_date: '2012-12-31')
+    comment = Comment.create(content: 'this a comment', user_id: User.current.id, project_id: 1, commentable_id: issue.id, commentable_type: 'Issue')
+    comment1 = Comment.create(content: 'this a comment', user_id: User.current.id, project_id: 1, commentable_id: issue.id, commentable_type: 'Issue')
+    comment2 = Comment.create(content: 'this a comment', user_id: User.current.id, project_id: 1, commentable_id: 666, commentable_type: 'Issue')
+    assert comment.save
+    assert comment1.save
+    assert comment2.save
+
+    Rorganize::Models::Commentable::bulk_delete_dependent([issue.id], 'Issue')
+    assert issue.reload
+    assert_raise(ActiveRecord::RecordNotFound) {comment.reload}
+    assert_raise(ActiveRecord::RecordNotFound) {comment1.reload}
+    assert comment2.reload
+  end
+end
