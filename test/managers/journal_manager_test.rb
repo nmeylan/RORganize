603bfbf8f201a9af636dd9fac11565240bcc1748
nmeylan <nmeylan@gmail.com>
2015-01-21 10:12:37 +0100

    Empty journal manager, because it has been tested in model behaviors.

---
+++ test/managers/journal_manager_test.rb
@@ -0,0 +1,7 @@
+# Author: Nicolas Meylan
+# Date: 21.01.15 10:02
+# Encoding: UTF-8
+# File: journal_manager_test.rb
+
+# Tested through :
+# Models => behaviors => journalizable and many other test files.
