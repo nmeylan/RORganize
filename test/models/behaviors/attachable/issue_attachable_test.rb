0fc1e86e79b0763575c7aa73b271964f0895e9e1
nmeylan <nmeylan@gmail.com>
2015-01-21 09:57:37 +0100

    Empty attachable test case. To do : test later.

---
+++ test/models/behaviors/attachable/issue_attachable_test.rb
@@ -0,0 +1,27 @@
+# Author: Nicolas Meylan
+# Date: 21.01.15 09:40
+# Encoding: UTF-8
+# File: issue_attachable_test.rb
+require 'test_helper'
+
+class IssueAttachableTest < ActiveSupport::TestCase
+
+  # Called before every test method runs. Can be used
+  # to set up fixture information.
+  def setup
+    # Do nothing
+  end
+
+  # Called after every test method runs. Can be used to tear
+  # down fixture information.
+
+  def teardown
+    # Do nothing
+  end
+
+  # Fake test
+  test "it fail" do
+
+    fail('Not implemented')
+  end
+end
