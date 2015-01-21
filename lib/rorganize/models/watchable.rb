b3fe1a564c1a7639936627a5fe72a02203314463
nmeylan <nmeylan@gmail.com>
2015-01-21 08:57:25 +0100

    Fix watchable behavior.

--- lib/rorganize/models/watchable.rb
+++ lib/rorganize/models/watchable.rb
@@ -11,57 +11,61 @@ module Rorganize
       end
 
       def watch_by?(user)
-        w = self.watchers.to_a.delete_if { |watcher| !watcher.user_id.eql?(user.id) }[0]
-        # w = Watcher.where(user_id: user.id, watchable_type: self.class.to_s, watchable_id: self.id).first
         is_a_project = self.is_a?(Project)
-        parent_watch = parent_watch_by?(user)
-        is_watched?(is_a_project, parent_watch, w)
+        w = Watcher.find_by_user_id_and_watchable_type_and_watchable_id(user.id, self.class.to_s, self.id)
+        is_parent_watched = parent_watch_by?(user)
+        is_watched?(is_a_project, is_parent_watched, w)
       end
 
-      def is_watched?(is_a_project, parent_watch, w)
-        (!is_a_project && (parent_is_watched?(parent_watch, w) || is_not_excluded(parent_watch, w))) || project_is_watched?(is_a_project, w)
+      def is_watched?(is_a_project, is_parent_watched, w)
+        (!is_a_project && (parent_is_watched?(is_parent_watched, w) || is_not_excluded(is_parent_watched, w))) ||
+            project_is_watched?(is_a_project, w)
       end
 
       def project_is_watched?(is_a_project, w)
         (is_a_project && !w.nil?)
       end
 
-      def is_not_excluded(parent_watch, w)
-        (!parent_watch && (w && !w.is_unwatch))
+      def is_not_excluded(is_parent_watched, w)
+        (!is_parent_watched && (w && !w.is_unwatch))
       end
 
-      def parent_is_watched?(parent_watch, w)
-        (parent_watch &&(w.nil? || (w && !w.is_unwatch)))
+      def parent_is_watched?(is_parent_watched, w)
+        (is_parent_watched &&(w.nil? || (w && !w.is_unwatch)))
       end
 
       def watcher_for(user)
-        w = self.watchers.to_a.delete_if { |watcher| !watcher.user_id.eql?(user.id) && watcher.is_unwatch }[0]
+        w = self.watchers.to_a.detect { |watcher| watcher.user_id.eql?(user.id) && !watcher.is_unwatch }
         w = Watcher.where(user_id: user.id, watchable_type: 'Project', watchable_id: self.project_id, is_unwatch: false).first if w.nil? && !self.is_a?(Project)
         w
       end
 
       def real_watchers
         unwatch = excluded_watchables
+        project_w = parent_watchers unless self.is_a?(Project)
         w = user_watchers
-        project_w = parent_watchers if !self.is_a?(Project)
         sum = project_w.to_a + w.to_a
         sum.flatten(0).delete_if { |watcher| unwatch.include? watcher.user_id }
       end
 
       def parent_watchers
-        Watcher.includes(author: :preferences).where(watchable_type: 'Project', watchable_id: self.project_id)
+        Watcher.includes(author: :preferences).where(watchable_type: 'Project', watchable_id: watchable_project_id)
       end
 
       def user_watchers
-        Watcher.includes(author: :preferences).where(watchable_type: self.class.to_s, watchable_id: self.id, project_id: self.project_id)
+        Watcher.includes(author: :preferences).where(watchable_type: self.class.to_s, watchable_id: self.id, project_id: watchable_project_id)
       end
 
       def excluded_watchables
-        Watcher.includes(author: :preferences).where(watchable_type: self.class.to_s, watchable_id: self.id, is_unwatch: true, project_id: self.project_id).pluck('user_id')
+        Watcher.includes(author: :preferences).where(watchable_type: self.class.to_s, watchable_id: self.id, is_unwatch: true, project_id: watchable_project_id).pluck('user_id')
       end
 
       def parent_watch_by?(user)
         !self.is_a?(Project) && Watcher.where(user_id: user.id, watchable_type: self.project.class.to_s, watchable_id: self.project.id).any?
+      end
+
+      def watchable_project_id
+        self.is_a?(Project) ? self.id : self.project_id
       end
 
       class << self
