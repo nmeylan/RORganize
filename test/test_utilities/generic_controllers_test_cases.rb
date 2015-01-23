29c8a1c7ef4a5ebb108d6fdd789a6e0018360cc6
nmeylan <nmeylan@gmail.com>
2015-01-23 12:16:30 +0100

    Generic controller test cases : should get 404 on method now handle format: :html

--- test/test_utilities/generic_controllers_test_cases.rb
+++ test/test_utilities/generic_controllers_test_cases.rb
@@ -8,8 +8,18 @@ module Rorganize
     def should_get_404_on(method, action, *args)
       send(method, action, *args)
       assert_response :missing
-      assert_not_nil @response.header["flash-error-message"]
-      assert @response.header["flash-error-message"].start_with?('Seems')
+      if format_js?(*args)
+        assert_not_nil @response.header["flash-error-message"]
+        assert @response.header["flash-error-message"].start_with?('Seems')
+      else
+       assert_select "title", "The page you were looking for doesn't exist (404)"
+      end
+    end
+
+    def format_js?(*args)
+      args = [{}] if args.first.nil?
+      hash = args.first
+      hash[:format] && hash[:format].eql?(:js)
     end
   end
 end
