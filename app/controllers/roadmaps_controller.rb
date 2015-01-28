f336042b82996cd891835592bbe77db4eb5152fd
nmeylan <nmeylan@gmail.com>
2015-01-28 14:56:44 +0100

    Fix typo : roadmap#show. Rename @version_decorator to @versions_decorator. Because we load a collection and not a single object.

--- app/controllers/roadmaps_controller.rb
+++ app/controllers/roadmaps_controller.rb
@@ -8,6 +8,7 @@ require 'roadmap/roadmap_report'
 class RoadmapsController < ApplicationController
   helper VersionsHelper
   include Rorganize::RichController::GanttCallbacks
+
   before_action { |c| c.add_action_alias = {'version' => 'show'} }
   before_action :check_permission, only: [:gantt, :manage_gantt, :show, :version]
   before_action { |c| c.menu_context :project_menu }
@@ -17,7 +18,7 @@ class RoadmapsController < ApplicationController
   #GET/project/:project_id/roadmaps
   def show
     roadmap = RoadmapReport.new(@project_decorator)
-    @version_decorator = roadmap.version_decorator
+    @versions_decorator = roadmap.versions_decorator
     old_versions = roadmap.old_versions
     respond_to do |format|
       format.html { render :index, locals: {old_versions: old_versions} }
@@ -25,23 +26,12 @@ class RoadmapsController < ApplicationController
   end
 
   def version
-    @version_decorator = Version.eager_load(issues: [:status, :tracker]).find_by_id(params[:id])
-    if @version_decorator
-      @version_decorator = @version_decorator.decorate
-    else
-      render_404
-    end
+    @version_decorator = Version.eager_load(issues: [:status, :tracker]).find_by!(id: params[:id], project_id: @project.id).decorate
   end
 
   private
   def find_project
-    @project_decorator = Project.eager_load(:versions, :attachments).where(slug: params[:project_id])[0]
-    if @project_decorator
-      @project_decorator = @project_decorator.decorate
-      @project = @project_decorator.model
-    else
-      render_404
-    end
+    @project = Project.eager_load(:versions, :attachments).find_by!(slug: params[:project_id])
+    @project_decorator = @project.decorate
   end
-
 end
--- app/query_objects/roadmap/roadmap_report.rb
+++ app/query_objects/roadmap/roadmap_report.rb
@@ -4,13 +4,13 @@
 # File: roadmap_report.rb
 
 class RoadmapReport
-  attr_reader :version_decorator, :old_versions
+  attr_reader :versions_decorator, :old_versions
   def initialize(project_decorator)
-    @version_decorator = project_decorator.model.versions.where(is_done: false).eager_load(issues: [:status, :tracker]).order(:position).decorate
+    @versions_decorator = project_decorator.model.versions.where(is_done: false).eager_load(issues: [:status, :tracker]).order(:position).decorate
     unplanned = Version.new(name: 'Unplanned')
     unplanned.issues << Issue.where(project_id: project_decorator.id, version_id: nil).eager_load(:status, :tracker)
     unplanned.project = project_decorator.model
-    @version_decorator.to_a << unplanned.decorate
+    @versions_decorator.to_a << unplanned.decorate
     @old_versions = project_decorator.old_versions.decorate
   end
 end
