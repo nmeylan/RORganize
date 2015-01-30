375026f790f92e7b571a352ddaf8ceb27630e53f
nmeylan <nmeylan@gmail.com>
2015-01-30 07:33:07 +0100

    Cover setting controller

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -4,41 +4,44 @@
 # File: settings_controller.rb
 
 class SettingsController < ApplicationController
-  include Rorganize::RichController
-  before_action :set_pagination, only: [:public_queries]
-  before_action :check_queries_permission, only: [:public_queries]
-  before_action :check_permission, except: [:public_queries, :delete_attachment, :update]
-  before_action { |c| c.menu_context :project_menu }
-  before_action { |c| c.menu_item(params[:controller]) }
-  before_action { |c| c.top_menu_item('projects') }
   helper QueriesHelper
   helper TrackersHelper
   include Rorganize::Managers::ModuleManager::ModuleManagerHelper
+  include Rorganize::RichController
+
+  before_action :set_pagination, only: [:public_queries]
+  before_action :check_queries_permission, only: [:public_queries]
+  before_action :check_permission, except: [:public_queries, :delete_attachment]
+  before_action { |c| c.menu_context :project_menu }
+  before_action { |c| c.menu_item(params[:controller]) }
+  before_action { |c| c.top_menu_item('projects') }
+
   #GET project/:project_identifier/settings/
   def index
-    tracker_ids = @project.trackers.collect { |tracker| tracker.id }
-    if @project.attachments.empty?
-      @project.attachments.build
-    end
-    @project_decorator = @project.decorate
-    @trackers_decorator = Tracker.all.decorate(context: {checked_ids: tracker_ids})
+    load_form_data
     respond_to do |format|
       format.html
     end
   end
 
   #POST project/:project_identifier/settings/
-  #POST project/:project_identifier/settings/
   def update
     @project.update_info(project_params, params[:trackers])
     respond_to do |format|
-      flash[:notice] = t(:successful_update)
-      format.html { redirect_to settings_path(@project.slug) }
+      if @project.errors.messages.empty?
+        success_generic_update_callback(format, settings_path(@project.slug), true)
+      else
+        @project.reload
+        load_form_data
+        format.html { render :index, status: :unprocessable_entity }
+      end
     end
   end
 
   def public_queries
-    @queries_decorator = Query.public_queries(@project.id).eager_load(:user).paginated(@sessions[:current_page], @sessions[:per_page], order('queries.name')).decorate(context: {queries_url: public_queries_settings_path(@project.slug), action_name: 'public_queries'})
+    @queries_decorator = Query.public_queries(@project.id).eager_load(:user)
+                             .paginated(@sessions[:current_page], @sessions[:per_page], order('queries.name'))
+                             .decorate(context: {queries_url: public_queries_settings_path(@project.slug), action_name: 'public_queries'})
     respond_to do |format|
       format.html
       format.js { respond_to_js }
@@ -46,7 +49,7 @@ class SettingsController < ApplicationController
   end
 
   def delete_attachment
-    attachment = Attachment.find(params[:id])
+    attachment = Attachment.find_by!(id: params[:id], attachable_id: @project.id, attachable_type: 'Project')
     if attachment.destroy
       @project.attachments.clear
       @project.attachments.build
@@ -59,17 +62,12 @@ class SettingsController < ApplicationController
 
   def modules
     if request.post?
-      @project.enabled_modules.clear
-      params['modules']['name'].each do |mod|
-        ary = mod.split('-')
-        m = EnabledModule.new(controller: ary[0], action: ary[1], name: ary[2])
-        @project.enabled_modules << m
-      end
-      @project.save
-      reload_enabled_module(@project.id)
+      @project.enable_modules(params['modules']['name'])
     end
     always_enabled = Rorganize::Managers::ModuleManager.always_enabled_module
-    @modules = Rorganize::Managers::ModuleManager.modules(:project).module_items.delete_if { |mod| always_enabled.any? { |modules| modules[:controller].eql?(mod.controller) && modules[:action].eql?(mod.action) } }
+    @modules = Rorganize::Managers::ModuleManager.modules(:project).module_items.delete_if do |mod|
+      always_enabled.any? { |modules| modules[:controller].eql?(mod.controller) && modules[:action].eql?(mod.action) }
+    end
     enabled_modules = @project.enabled_modules.collect { |mod| mod.name }
     respond_to do |format|
       format.html { render :modules, locals: {enabled_modules: enabled_modules} }
@@ -89,4 +87,12 @@ class SettingsController < ApplicationController
     params.require(:project).permit(Project.permit_attributes)
   end
 
+  def load_form_data
+    tracker_ids = @project.trackers.collect { |tracker| tracker.id }
+    if @project.attachments.empty?
+      @project.attachments.build
+    end
+    @project_decorator = @project.decorate
+    @trackers_decorator = Tracker.all.decorate(context: {checked_ids: tracker_ids})
+  end
 end
--- test/controllers/settings_controller_test.rb
+++ test/controllers/settings_controller_test.rb
@@ -4,7 +4,7 @@ class SettingsControllerTest < ActionController::TestCase
   # Called before every test method runs. Can be used
   # to set up fixture information.
   def setup
-
+    @project = projects(:projects_001)
     # Do nothing
   end
 
@@ -15,4 +15,79 @@ class SettingsControllerTest < ActionController::TestCase
     # Do nothing
   end
 
+  test "should access to index of settings" do
+    get_with_permission :index
+    assert_response :success
+    assert_not_nil assigns(:project_decorator)
+    assert_not_nil assigns(:trackers_decorator)
+  end
+
+  test "should update project information" do
+    assert_equal 2, @project.trackers.count
+    put_with_permission :update, project: {name: 'New project name', is_public: 1, description: "A simple description"}, trackers: {}, id: "update_project_informations"
+
+    @project.reload
+    assert_equal 0, @project.trackers.count
+    assert_equal 'New project name', @project.name
+    assert @project.is_public, 'Project is private'
+
+    assert_redirected_to settings_path(@project.slug)
+  end
+
+  test "should refresh the page when update project failed" do
+    put_with_permission :update, project: {name: '', is_public: 1, description: "A simple description"},
+                        trackers: {}, id: "update_project_informations"
+    @project.reload
+
+    assert_not_nil assigns(:project_decorator)
+    assert_not_nil assigns(:trackers_decorator)
+    assert_response :unprocessable_entity
+  end
+
+  test "should access to project public queries" do
+    allow_user_to('index', 'queries')
+    _get :public_queries
+    assert_response :success
+    assert_not_nil assigns(:queries_decorator)
+  end
+
+  test "should destroy project attachment" do
+    attachment = Attachment.new(name: 'File test', file_file_name: 'file_test.png',
+                                attachable_id: @project.id, attachable_type: 'Project')
+    @project.attachments << attachment
+    @project.save
+    assert_equal 1, @project.attachments.count
+
+    _delete :delete_attachment, id: attachment.id, format: :js
+    @project.reload
+    assert_equal 0, @project.attachments.count
+    assert_response :success
+  end
+
+  test "should access to project modules" do
+    get_with_permission :modules
+    assert_response :success
+    assert_not_nil assigns(:modules)
+  end
+
+  test "should enable modules" do
+    post_with_permission :modules, modules: {name: ["projects-activity-activity", "roadmaps-show-roadmaps"]}
+    @project.reload
+    assert_equal 2, @project.enabled_modules.count
+    assert_response :success
+    assert_not_nil assigns(:modules)
+  end
+
+  # Forbidden actions
+  test "should get a 403 error when user is not allowed to access to index of settings" do
+    should_get_403_on(:_get, :index)
+  end
+
+  test "should get a 403 error when user is not allowed to access to modules" do
+    should_get_403_on(:_put, :modules)
+  end
+
+  test "should get a 403 error when user is not allowed to access to queries" do
+    should_get_403_on(:_get, :public_queries)
+  end
 end
