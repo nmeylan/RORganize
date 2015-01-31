a9bbf218070c9e5b869827e19d848b6a060507d7
nmeylan <nmeylan@gmail.com>
2015-01-31 15:54:55 +0100

    Fix missing permission to rename.

--- db/migrate/20140905193525_update_permission_name.rb
+++ db/migrate/20140905193525_update_permission_name.rb
@@ -1,5 +1,6 @@
 class UpdatePermissionName < ActiveRecord::Migration
   def up
-    Permission.where(controller: 'Settings', action: 'modules').first.update_column(:name, 'Manage modules')
+    permission_to_rename = Permission.where(controller: 'Settings', action: 'modules').first
+    permission_to_rename.update_column(:name, 'Manage modules') unless permission_to_rename.nil?
   end
 end
