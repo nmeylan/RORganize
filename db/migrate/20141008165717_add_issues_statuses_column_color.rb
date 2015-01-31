1b7514256d7e2f919499f1d6e38c096841a0dd89
nmeylan <nmeylan@gmail.com>
2015-01-31 16:18:39 +0100

    Be sure data exist before to update them in migration file.

--- db/migrate/20141008165717_add_issues_statuses_column_color.rb
+++ db/migrate/20141008165717_add_issues_statuses_column_color.rb
@@ -2,12 +2,25 @@ class AddIssuesStatusesColumnColor < ActiveRecord::Migration
   def change
     add_column :issues_statuses, :color, :string
 
-    IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'New')).update_attribute(:color, '#6cc644')
-    IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Redo')).update_attribute(:color, '#6cc644')
-    IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'In progress')).update_attribute(:color, '#fbca04')
-    IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Fixed to test')).update_attribute(:color, '#fbca04')
-    IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Not satisfying')).update_attribute(:color, '#fbca04')
-    IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Closed')).update_attribute(:color, '#bd2c00')
-    IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Tested to be delivered')).update_attribute(:color, '#bd2c00')
+    issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'New'))
+    issue_status.update_attribute(:color, '#6cc644') unless issue_status.nil?
+
+    issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Redo'))
+    issue_status.update_attribute(:color, '#6cc644') unless issue_status.nil?
+
+    issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'In progress'))
+    issue_status.update_attribute(:color, '#fbca04') unless issue_status.nil?
+
+    issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Fixed to test'))
+    issue_status.update_attribute(:color, '#fbca04') unless issue_status.nil?
+
+    issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Not satisfying'))
+    issue_status.update_attribute(:color, '#fbca04') unless issue_status.nil?
+
+    issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Closed'))
+    issue_status.update_attribute(:color, '#bd2c00') unless issue_status.nil?
+
+    issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Tested to be delivered'))
+    issue_status.update_attribute(:color, '#bd2c00') unless issue_status.nil?
   end
 end
