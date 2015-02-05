b7c0e4faf880a9a2d839ab00060fafc8e8b22921
nmeylan <nmeylan@gmail.com>
2015-02-05 16:22:16 +0100

    Auto enable all project modules in decorator test cases.

--- test/test_utilities/decorator_test_case.rb
+++ test/test_utilities/decorator_test_case.rb
@@ -33,6 +33,7 @@ module Rorganize
       setup do
         Draper::ViewContext.current = @controller.view_context
         User.stubs(:current).returns(users(:users_001))
+        User.any_instance.stubs(:module_enabled?).returns(true)
         sign_in User.current
         drop_all_user_permissions
         @output_buffer = ActiveSupport::SafeBuffer.new ''
