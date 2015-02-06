a0ef970f4cb5db694b9c8875c8fd40eccc668317
nmeylan <nmeylan@gmail.com>
2015-02-06 15:20:22 +0100

    Cover issues status and issues statuses decorators.

--- test/decorators/issues_status_decorator_test.rb
+++ test/decorators/issues_status_decorator_test.rb
@@ -1,4 +1,65 @@
 require 'test_helper'
 
 class IssuesStatusDecoratorTest < Rorganize::Decorator::TestCase
+  def setup
+    @status_decorator = issues_statuses(:issues_statuses_001).decorate
+  end
+
+  test "it displays a link to edit when user is allowed to" do
+    allow_user_to('edit')
+    node(@status_decorator.edit_link)
+    assert_select 'a', 1
+    assert_select 'a[href=?]', edit_issues_status_path(@status_decorator.id)
+  end
+
+  test "it displays a link to delete when user is allowed to" do
+    allow_user_to('destroy')
+    node(@status_decorator.delete_link)
+    assert_select 'a', 1
+    assert_select 'a[href=?]', issues_status_path(@status_decorator.id)
+  end
+
+  test "it should not have a link to edit action when user is not allowed to" do
+    node(@status_decorator.edit_link)
+    assert_select 'span', 1
+    assert_select 'span', text: 'New'
+  end
+
+  test "it should not have a link to delete action when user is not allowed to" do
+    node(@status_decorator.delete_link)
+    assert_nil @node
+  end
+
+  test "it displays a link to increment the status position" do
+    assert_equal 7, @status_decorator.position
+    node(@status_decorator.inc_position_link)
+    assert_select 'a', 1
+    assert_select 'a.change-position', 1
+  end
+
+  test "it displays a disabled link to increment the status position when position is already the top" do
+    @status_decorator.enumeration.position = 1
+    assert_equal 1, @status_decorator.position
+    node(@status_decorator.inc_position_link)
+    assert_select 'a', 1
+    assert_select 'a.change-position', 0
+    assert_select 'a.icon-disabled-up-arrow', 1
+  end
+
+  test "it displays a link to decrement the status position" do
+    collection_size = IssuesStatus.all.count
+    assert_not_equal collection_size, @status_decorator.position
+    node(@status_decorator.dec_position_link(collection_size))
+    assert_select 'a', 1
+    assert_select 'a.change-position', 1
+  end
+
+  test "it displays a disabled link to decrement the status position when position is already the top" do
+    collection_size = IssuesStatus.all.count
+    @status_decorator.enumeration.position = collection_size
+    node(@status_decorator.dec_position_link(collection_size))
+    assert_select 'a', 1
+    assert_select 'a.change-position', 0
+    assert_select 'a.icon-disabled-down-arrow', 1
+  end
 end
--- test/decorators/issues_statuses_decorator_test.rb
+++ test/decorators/issues_statuses_decorator_test.rb
@@ -1,4 +1,35 @@
 require 'test_helper'
 
 class IssuesStatusesDecoratorTest < Rorganize::Decorator::TestCase
+  def setup
+    helpers.instance_eval('view_context').stubs(:sortable).returns('')
+    helpers.stubs(:session).returns({issues_statuses: {current_page: 1}})
+
+    @issues_statuses_decorator = IssuesStatus.all.decorate
+  end
+
+  test "it displays no data when collection is empty" do
+    IssuesStatus.delete_all
+    node(@issues_statuses_decorator.display_collection)
+    assert_select '#issues-statuses-content', 1
+    assert_select 'h3', I18n.t(:text_no_data)
+  end
+
+  test "it displays a table when collection contains entries" do
+    @node = node @issues_statuses_decorator.display_collection
+    assert_select '#issues-statuses-content', 1
+    assert_select 'table', 1
+  end
+
+  test "it has a link to new action when user is allowed to" do
+    allow_user_to('new')
+    node(@issues_statuses_decorator.new_link)
+    assert_select 'a', 1
+    assert_select 'a[href=?]', "#{issues_statuses_path}/new"
+  end
+
+  test "it should not have a link to new action when user is not allowed to" do
+    node(@issues_statuses_decorator.new_link)
+    assert_nil @node
+  end
 end
