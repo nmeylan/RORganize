fadde1d8a4a61b43bd70befa97fddd6d83b96d5f
nmeylan <nmeylan@gmail.com>
2015-02-06 22:42:53 +0100

    Cover members decorators.

--- test/decorators/member_decorator_test.rb
+++ test/decorators/member_decorator_test.rb
@@ -1,4 +1,31 @@
 require 'test_helper'
 
 class MemberDecoratorTest < Rorganize::Decorator::TestCase
+  def setup
+    @project = projects(:projects_001)
+    @member_decorator = User.current.member_for(@project.id).decorate(context: {project: @project})
+  end
+
+  test "it display a role select box with the current member role selected when user is allowed to member change role" do
+    allow_user_to('change_role')
+    node(@member_decorator.role_selection(Role.all))
+    assert_select 'select', 1
+    assert_select 'select[data-link=?]', change_role_members_path(@project.slug, @member_decorator.id)
+    assert_select 'option[selected]', text: @member_decorator.role.caption
+  end
+
+  test "it display role name when user is allowed to member change role" do
+    assert_equal @member_decorator.role.caption, @member_decorator.role_selection(Role.all)
+  end
+
+  test "it displays a link to delete when user is allowed to" do
+    allow_user_to('destroy')
+    node(@member_decorator.delete_link)
+    assert_select 'a', 1
+    assert_select 'a[href=?]', member_path(@project.slug, @member_decorator.id)
+  end
+
+  test "it should not display a link to delete when user is not allowed to" do
+    assert_nil @member_decorator.delete_link
+  end
 end
--- test/decorators/members_decorator_test.rb
+++ test/decorators/members_decorator_test.rb
@@ -1,4 +1,41 @@
 require 'test_helper'
 
 class MembersDecoratorTest < Rorganize::Decorator::TestCase
+
+  def setup
+    helpers.instance_eval('view_context').stubs(:sortable).returns('')
+    helpers.stubs(:session).returns({members: {current_page: 1}})
+
+    @project = projects(:projects_001)
+    @members_decorator = @project.members.decorate(context: {project: @project})
+  end
+
+  test "it displays no data when collection is empty" do
+    @project.members.clear
+    node(@members_decorator.display_collection)
+    assert_select '#members-content', 1
+    assert_select 'h3', I18n.t(:text_no_data)
+  end
+
+  test "it displays a table when collection contains entries" do
+    @project.members.clear
+    @project.members << Member.create(user_id: User.current.id, project_id: @project.id, role_id: roles(:roles_001).id)
+
+    @members_decorator = @project.members.decorate(context: {project: @project})
+    @node = node @members_decorator.display_collection
+    assert_select '#members-content', 1
+    assert_select 'table', 1
+  end
+
+  test "it has a link to new action when user is allowed to" do
+    allow_user_to('new')
+    node(@members_decorator.new_link)
+    assert_select 'a', 1
+    assert_select 'a[href=?]', "#{members_path(@project.slug)}/new"
+  end
+
+  test "it should not have a link to new action when user is not allowed to" do
+    node(@members_decorator.new_link)
+    assert_nil @node
+  end
 end
