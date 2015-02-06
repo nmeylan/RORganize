26fe5a433832d46afe47d81d1c15a226ede98d4c
nmeylan <nmeylan@gmail.com>
2015-02-06 23:54:56 +0100

    Cover permissions and permission controller.

--- test/decorators/permission_decorator_test.rb
+++ test/decorators/permission_decorator_test.rb
@@ -1,4 +1,37 @@
 require 'test_helper'
 
 class PermissionDecoratorTest < Rorganize::Decorator::TestCase
+  def setup
+    @permission_decorator = Permission.create(action: 'new', controller: 'controller', name: 'New', is_locked: false).decorate
+  end
+
+  test "it displays a link to edit when user is allowed to" do
+    allow_user_to('edit')
+    node(@permission_decorator.edit_link)
+    assert_select 'a', 1
+    assert_select 'a[href=?]', edit_permission_path(@permission_decorator.id)
+  end
+
+  test "it displays a link to delete when user is allowed to" do
+    allow_user_to('destroy')
+    node(@permission_decorator.delete_link)
+    assert_select 'a', 1
+    assert_select 'a[href=?]', permission_path(@permission_decorator.id)
+  end
+
+  test "it should not have a link to edit action when user is not allowed to" do
+    assert_equal 'New', @permission_decorator.edit_link
+  end
+
+  test "it should not have a link to delete action when user is not allowed to" do
+    node(@permission_decorator.delete_link)
+    assert_nil @node
+  end
+
+  test "it should not have a link to delete action when permission is locked" do
+    allow_user_to('destroy')
+    @permission_decorator.is_locked = true
+    node(concat @permission_decorator.delete_link)
+    assert_select 'span.octicon-lock', 1
+  end
 end
--- test/decorators/permissions_decorator_test.rb
+++ test/decorators/permissions_decorator_test.rb
@@ -1,4 +1,34 @@
 require 'test_helper'
 
 class PermissionsDecoratorTest < Rorganize::Decorator::TestCase
+  def setup
+    @role = roles(:roles_001)
+
+    helpers.instance_eval('view_context').stubs(:sortable).returns('')
+    helpers.stubs(:session).returns({permissions: {current_page: 1}})
+
+    permissions = Permission.all
+    @permissions_decorator = permissions.decorate(context: {role_name: @role.caption, controller_list: {misc: permissions.collect(&:controller)}})
+  end
+
+  test "it displays a table when collection contains entries" do
+    @controller.request.path_parameters[:role_name] = @role.caption
+    @node = node @permissions_decorator.display_collection
+    assert_select 'form', 1
+    assert_select '#misc-tab', 1
+    assert_select '#project-tab', 1
+    assert_select '#administration-tab', 1
+  end
+
+  test "it displays a link to new action when user is allowed to" do
+    allow_user_to('new')
+    node(@permissions_decorator.new_link)
+    assert_select 'a', 1
+    assert_select 'a[href=?]', "#{permissions_path}/new"
+  end
+
+  test "it should not have a link to new action when user is not allowed to" do
+    node(@permissions_decorator.new_link)
+    assert_nil @node
+  end
 end
