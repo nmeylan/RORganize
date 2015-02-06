3dc75d59b3a2dbc2726001182b644600ec0b4994
nmeylan <nmeylan@gmail.com>
2015-02-06 16:51:53 +0100

    We can now specify a controller class for the decorator test case context. If no controller is specified then try to get the controller class with the test case name. If no controller is found, then use ApplicationController.

--- test/test_utilities/decorator_test_case.rb
+++ test/test_utilities/decorator_test_case.rb
@@ -25,18 +25,14 @@ module Rorganize
         super(test_case)
         @node = nil #should be defined just before calling assert_select
         @routes = Rails.application.routes
-        @controller_class = self.class.determine_default_controller_class(self.class.name)
-        @controller = @controller_class.new
-        @controller.request = ActionController::TestRequest.new
-        @controller_name = @controller.controller_name
-        @controller.instance_variable_set(:@sessions, {@controller_name.to_sym => {}})
+        initialize_controller_context
       end
 
       setup do
         Draper::ViewContext.current = @controller.view_context
         User.stubs(:current).returns(users(:users_001))
         User.any_instance.stubs(:module_enabled?).returns(true)
-        User.any_instance.stubs(:avatar).returns(Avatar.new(name: 'avatar', attachable_id:  User.current.id, attachable_type: 'User'))
+        User.any_instance.stubs(:avatar).returns(Avatar.new(name: 'avatar', attachable_id: User.current.id, attachable_type: 'User'))
         sign_in User.current
         drop_all_user_permissions
         @output_buffer = ActiveSupport::SafeBuffer.new ''
@@ -51,7 +47,20 @@ module Rorganize
         @node #should be defined just before calling assert_select
       end
 
+      def initialize_controller_context
+        @controller_class = self.class.controller_class ? self.class.controller_class : self.class.determine_default_controller_class(self.class.name)
+        @controller = @controller_class.nil? ? ApplicationController.new : @controller_class.new
+        @controller.request = ActionController::TestRequest.new
+        @controller_name = @controller.controller_name
+        @controller.instance_variable_set(:@sessions, {@controller_name.to_sym => {}})
+      end
+
       class << self
+        attr_accessor :controller_class
+        def set_controller_class(controller_class)
+          self.controller_class = controller_class
+        end
+
         def determine_default_controller_class(name)
           determine_constant_from_test_name(name) do |constant|
             Class === constant && constant < ActionController::Metal
