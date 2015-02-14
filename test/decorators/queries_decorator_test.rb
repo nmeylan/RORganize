524213e06690fcb5b4401180492d93b766444843
nmeylan <nmeylan@gmail.com>
2015-02-14 21:47:22 +0100

    Cover query and queries decorator.

--- test/decorators/queries_decorator_test.rb
+++ test/decorators/queries_decorator_test.rb
@@ -1,4 +1,37 @@
 require 'test_helper'
 
 class QueriesDecoratorTest < Rorganize::Decorator::TestCase
+  def setup
+    @project = projects(:projects_001)
+    Query.create!(author_id: User.current.id, project_id: @project.id,
+                  stringify_query: 'aaa', stringify_params: 'params',
+                  object_type: 'Issue', name: 'my query')
+
+    helpers.instance_eval('view_context').stubs(:sortable).returns('')
+    helpers.instance_eval('view_context').instance_variable_set(:@project, @project)
+    helpers.stubs(:session).returns({queries: {current_page: 1}})
+
+    @queries_decorator = Query.all.decorate(context: {queries_url: queries_path, action_name: 'index'})
+  end
+
+  test "it displays no data when collection is empty" do
+    @queries_decorator = Query.where('1=2').decorate(context: {queries_url: queries_path, action_name: 'index'})
+    node(@queries_decorator.display_collection)
+    assert_select '#queries-content', 1
+    assert_select 'h3', I18n.t(:text_no_data)
+  end
+
+  test "it displays a table when collection contains entries" do
+    node(@queries_decorator.display_collection)
+    assert_select '#queries-content', 1
+    assert_select 'table', 1
+  end
+
+  test "it override default pagination path" do
+    assert_equal queries_path, @queries_decorator.pagination_path
+  end
+
+  test "it override default sortable action" do
+    assert_equal 'index', @queries_decorator.sortable_action
+  end
 end
--- test/decorators/query_decorator_test.rb
+++ test/decorators/query_decorator_test.rb
@@ -1,4 +1,49 @@
 require 'test_helper'
 
 class QueryDecoratorTest < Rorganize::Decorator::TestCase
+  def setup
+    @project = projects(:projects_001)
+    @query_decorator = Query.create!(author_id: User.current.id, project_id: @project.id,
+                                        stringify_query: 'aaa', stringify_params: 'params',
+                                        object_type: 'Issue', name: 'my query').decorate
+
+  end
+
+  test "it displays a link to edit when user is allowed to" do
+    allow_user_to('edit', 'queries')
+    node(@query_decorator.edit_link)
+    assert_select 'a', 1
+    assert_select 'a[href=?]', edit_query_path(@query_decorator.id)
+  end
+
+  test "it displays a link to view query when user is allowed to" do
+    allow_user_to('show', 'queries')
+    node(@query_decorator.show_link)
+    assert_select 'a', 1
+    assert_select 'a[href=?]', query_path(@query_decorator.id)
+  end
+
+  test "it displays a link to delete when user is allowed to" do
+    allow_user_to('destroy')
+    node(@query_decorator.delete_link)
+    assert_select 'a', 1
+    assert_select 'a[href=?]', query_path(@query_decorator.id)
+  end
+
+  test "it displays the user name" do
+    assert_equal 'Nicolas Meylan', @query_decorator.author
+  end
+
+  test "it should not have a link to edit action when user is not allowed to" do
+    node(@query_decorator.edit_link)
+  end
+
+  test "it should just display the caption of the query when user is not allowed to view it" do
+    assert_equal 'my query', @query_decorator.show_link
+  end
+
+  test "it should not have a link to delete action when user is not allowed to" do
+    node(@query_decorator.delete_link)
+    assert_nil @node
+  end
 end
