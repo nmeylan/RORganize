b524957a19a1f43898ff7b2061ab055ae4ff6730
nmeylan <nmeylan@gmail.com>
2015-02-14 21:59:02 +0100

    Cover role and roles decorators.

--- test/decorators/role_decorator_test.rb
+++ test/decorators/role_decorator_test.rb
@@ -1,4 +1,40 @@
 require 'test_helper'
 
 class RoleDecoratorTest < Rorganize::Decorator::TestCase
+  def setup
+    @role_decorator = Role.create(name: 'Test role', is_locked: false).decorate
+  end
+
+  test "it displays a link to edit when user is allowed to" do
+    allow_user_to('edit')
+    node(@role_decorator.edit_link)
+    assert_select 'a', 1
+    assert_select 'a[href=?]', edit_role_path(@role_decorator.id)
+  end
+
+  test "it displays a link to delete when user is allowed to" do
+    allow_user_to('destroy')
+    node(@role_decorator.delete_link)
+    assert_select 'a', 1
+    assert_select 'a[href=?]', role_path(@role_decorator.id)
+  end
+
+  test "it should not have a link to edit action when user is not allowed to" do
+    node(@role_decorator.edit_link)
+    assert_select 'span', 1
+    assert_select 'span', text: 'Test role'
+  end
+
+  test "it should not have a link to edit action when user is allowed but role is locked" do
+    allow_user_to('edit')
+    @role_decorator.is_locked = true
+    node(@role_decorator.edit_link)
+    assert_select 'span', 1
+    assert_select 'span', text: 'Test role'
+  end
+
+  test "it should not have a link to delete action when user is not allowed to" do
+    node(@role_decorator.delete_link)
+    assert_nil @node
+  end
 end
--- test/decorators/roles_decorator_test.rb
+++ test/decorators/roles_decorator_test.rb
@@ -1,4 +1,35 @@
 require 'test_helper'
 
 class RolesDecoratorTest < Rorganize::Decorator::TestCase
+  def setup
+    helpers.instance_eval('view_context').stubs(:sortable).returns('')
+    helpers.stubs(:session).returns({roles: {current_page: 1}})
+
+    @roles_decorator = Role.all.decorate
+  end
+
+  test "it displays no data when collection is empty" do
+    @roles_decorator = Role.where('1=2').decorate
+    node(@roles_decorator.display_collection)
+    assert_select '#roles-content', 1
+    assert_select 'h3', I18n.t(:text_no_roles)
+  end
+
+  test "it displays a table when collection contains entries" do
+    @node = node @roles_decorator.display_collection
+    assert_select '#roles-content', 1
+    assert_select 'table', 1
+  end
+
+  test "it displays a link to new action when user is allowed to" do
+    allow_user_to('new')
+    node(@roles_decorator.new_link)
+    assert_select 'a', 1
+    assert_select 'a[href=?]', "#{roles_path}/new"
+  end
+
+  test "it should not have a link to new action when user is not allowed to" do
+    node(@roles_decorator.new_link)
+    assert_nil @node
+  end
 end
