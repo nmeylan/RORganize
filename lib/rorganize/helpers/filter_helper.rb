e8110d51bd501838d4be94345f6dca44c4235c8e
nmeylan <nmeylan@gmail.com>
2015-02-23 18:11:13 +0100

    Cover filter helper. Remove empty test case.

--- lib/rorganize/helpers/filter_helper.rb
+++ lib/rorganize/helpers/filter_helper.rb
@@ -5,12 +5,12 @@
 module Rorganize
   module Helpers
     module FilterHelper
-# Build a filter form for given criteria.
-# @param [String] label : what is filtered (e.g : issues, documents).
-# @param [Array] filtered_attributes : an array of filtered attribute @see Document.filtered_attributes.
-# @param [String] submission_path : the path to controller when the filter form is submit.
-# @param [Boolean] can_save : false when save button is hidden, true otherwise.
-# @param [hash] save_button_options
+      # Build a filter form for given criteria.
+      # @param [String] label : what is filtered (e.g : issues, documents).
+      # @param [Array] filtered_attributes : an array of filtered attribute @see Document.filtered_attributes.
+      # @param [String] submission_path : the path to controller when the filter form is submit.
+      # @param [Boolean] can_save : false when save button is hidden, true otherwise.
+      # @param [hash] save_button_options
       def filter_tag(label, filtered_attributes, submission_path, can_save = false, save_button_options = {})
         content_tag :fieldset, id: "#{label}-filter" do
           concat content_tag :legend, link_to(glyph(t(:link_filter), 'chevron-right'), '#', {class: 'icon-collapsed toggle', id: "#{label}"})
@@ -41,9 +41,11 @@ module Rorganize
 
       # Build a save button for filter : based on user permissions (does user is allowed to create custom queries?)
       # @param [Boolean] can_save : false when save button is hidden, true otherwise.
-      # @param [Array] filtered_content : an array of previous submitted filter, it use because if nothing were filtered then whe don't display the button.
-      # @param [User] user : the current user.
-      # @param [Project] project : current project.
+      # @param [Hash] options : a hash that contains those following keys :
+      # :filtered_content : a stringified JSON object of previous submitted filter, it use because if nothing were filtered then whe don't display the button.
+      # :user : the current user.
+      # :project : current project.
+      # :type : Query type. (E.g : 'Issue')
       def save_filter_button_tag(can_save, options)
         project = options[:project]
         user = options[:user]
@@ -56,9 +58,11 @@ module Rorganize
       end
 
       def can_user_save_query?(can_save, filter_content, project, user, create_or_edit)
-        can_save && !filter_content.eql?('') && user.allowed_to?('new', 'Queries', project) && create_or_edit
+        can_save && filter_content && !filter_content.empty? && user.allowed_to?('new', 'Queries', project) && create_or_edit
       end
 
+      # @param [Array] filtered_attributes : an array that contains all filtered attribute names.
+      # E.g : ['Author', 'Status', 'Created at']
       def filter_attribute_choice_tag(filtered_attributes)
         content_tag :div, class: 'autocomplete-combobox nosearch no-padding-left no-height' do
           select_tag 'filters_list', options_for_select(filtered_attributes), class: 'chzn-select cbb-verylarge', id: 'filters-list', multiple: true
@@ -81,6 +85,16 @@ module Rorganize
       # @param [Array] options_for_radio : this array must contains one or more of these values
       # ('all', 'contains', 'not_contains', 'equal', 'superior', 'inferior', 'different', 'today', 'open', 'close').
       # @param [Object] args : for simple_select are  ('options_for_select', 'multiple', 'size').
+      #
+      #
+      # Some samples:
+      # generic_filter(:simple_select, 'Author', 'author', %w(all equal not\ equal), %w(Nicolas James Sean Alex Boris))
+      # generic_filter(:simple_select, 'Done', 'done', %w(all equal not\ equal superior inferior), %w(10 40 60 80 100), false, 'cbb-small')
+      #
+      # generic_filter(:date, 'Created at', 'created_at', hash_for_radio['created'])
+      #
+      # generic_filter(:text, 'Subject', 'subject', hash_for_radio['subject'])
+      # @See more in issues_filter_helper.rb or documents_filter_helper.rb
       def generic_filter(filter_type, label, name, options_for_radio, *args)
         types = %w(:simple_select :date :text)
         label ||= name.capitalize
@@ -104,9 +118,10 @@ module Rorganize
 
       # For following filter: e.g: Assigned with 3 radio button (All, equal, different) and 1 combo
       # @param [String] name : name of the input field.
-      # @param [Object] options_for_select : options for select.
+      # @param [Array] options_for_select : options for select.
       # @param [Boolean] multiple : true multiple select enabled, disabled otherwise.
-      # @param [Object] size
+      # @param [String] size : the css class to define the select tag size. Default it's large.
+      # Available classes are : 'cbb-small', 'cbb-tiny', 'cbb-large', 'cbb-medium'
       def generics_filter_simple_select(name, options_for_select, multiple = true, size = nil)
         size ||= 'cbb-large'
         content_tag :div, class: 'autocomplete-combobox nosearch no-padding-left no-height' do
---
+++ test/helpers/filter_helper_test.rb
@@ -0,0 +1,213 @@
+# Author: Nicolas Meylan
+# Date: 23.02.15 15:51
+# Encoding: UTF-8
+# File: filter_helper_test.rb
+require 'test_helper'
+
+module Rorganize
+  module Helpers
+    class FilterHelperTest < Rorganize::Helpers::TestCase
+      test 'it builds radio buttons' do
+        buttons = %w(open close equal not\ equal)
+        node(generics_filter_radio_button('status', buttons))
+        assert_select 'span', 1
+
+        assert_select 'label', 4
+        assert_select 'label', text: 'Open'
+        assert_select 'label', text: 'Close'
+        assert_select 'label', text: 'Equal'
+        assert_select 'label', text: 'Not equal'
+
+        assert_select 'input[type=radio]', 4
+        assert_select 'input[type=radio][name="filter[status][operator]"]', 4
+        assert_select 'input[type=radio].status', 4
+        assert_select 'input[type=radio]#status_open', 1
+        assert_select 'input[type=radio]#status_close', 1
+        assert_select 'input[type=radio]#status_equal', 1
+        assert_select 'input[type=radio]#status_not_equal', 1
+      end
+
+      test 'it builds a filter date field' do
+        node(generics_filter_date_field('created_at'))
+
+        assert_select 'input[type=date]', 1
+        assert_select 'input[type=date][name="filter[created_at][value]"]', 1
+        assert_select 'input[type=date].calendar', 1
+      end
+
+      test 'it builds a filter text field' do
+        node(generics_filter_text_field('subject'))
+
+        assert_select 'input[type=text]', 1
+        assert_select 'input[type=text][name="filter[subject][value]"]', 1
+        assert_select 'input[type=text][size=?]', '80'
+      end
+
+      test 'it builds a filter select field' do
+        node(generics_filter_simple_select('author', %w(Nicolas James Sean Alex Boris)))
+
+        assert_select 'select', 1
+        assert_select 'select.chzn-select.cbb-large', 1
+        assert_select 'select#author_list', 1
+        assert_select 'select[multiple=?]', 'multiple'
+        assert_select 'option', 5
+        assert_select 'option', text: 'Nicolas'
+        assert_select 'option', text: 'James'
+        assert_select 'option', text: 'Sean'
+        assert_select 'option', text: 'Alex'
+        assert_select 'option', text: 'Boris'
+      end
+
+      test 'it builds a polymorphic filter for a simple select type' do
+        node(generic_filter(:simple_select, 'Author', 'author', %w(all equal not\ equal), %w(Nicolas James Sean Alex Boris)))
+
+        assert_select 'input[type=radio]', 3
+        assert_select 'input[type=radio][name="filter[author][operator]"]', 3
+        assert_select 'input[type=radio].author', 3
+        assert_select 'input[type=radio]#author_all', 1
+        assert_select 'input[type=radio]#author_equal', 1
+        assert_select 'input[type=radio]#author_not_equal', 1
+
+
+        assert_select 'select', 1
+        assert_select 'select.chzn-select.cbb-large', 1
+        assert_select 'select#author_list', 1
+        assert_select 'select[multiple=?]', 'multiple'
+        assert_select 'option', 5
+        assert_select 'option', text: 'Nicolas'
+        assert_select 'option', text: 'James'
+        assert_select 'option', text: 'Sean'
+        assert_select 'option', text: 'Alex'
+        assert_select 'option', text: 'Boris'
+      end
+
+      test 'it builds a polymorphic filter for a date type' do
+        node(generic_filter(:date, 'Created at', 'created_at', %w(all today superior inferior equal)))
+
+        assert_select 'input[type=radio]', 5
+        assert_select 'input[type=radio][name="filter[created_at][operator]"]', 5
+        assert_select 'input[type=radio].created_at', 5
+        assert_select 'input[type=radio]#created_at_all', 1
+        assert_select 'input[type=radio]#created_at_today', 1
+        assert_select 'input[type=radio]#created_at_superior', 1
+        assert_select 'input[type=radio]#created_at_inferior', 1
+        assert_select 'input[type=radio]#created_at_equal', 1
+
+        assert_select 'input[type=date]', 1
+        assert_select 'input[type=date][name="filter[created_at][value]"]', 1
+        assert_select 'input[type=date].calendar', 1
+      end
+
+      test 'it builds a polymorphic filter for a text type' do
+        node(generic_filter(:text, 'Subject', 'subject', %w(all contains not\ contains)))
+
+        assert_select 'input[type=radio]', 3
+        assert_select 'input[type=radio][name="filter[subject][operator]"]', 3
+        assert_select 'input[type=radio].subject', 3
+        assert_select 'input[type=radio]#subject_all', 1
+        assert_select 'input[type=radio]#subject_contains', 1
+        assert_select 'input[type=radio]#subject_not_contains', 1
+
+        assert_select 'input[type=text]', 1
+        assert_select 'input[type=text][name="filter[subject][value]"]', 1
+        assert_select 'input[type=text][size=?]', '80'
+      end
+
+      test 'it should raise an exception when filter type is undefined' do
+        assert_raises(Exception) { generic_filter(:undefined, 'Subject', 'subject', %w(all contains not\ contains)) }
+      end
+
+      test 'it builds a filter choice bar' do
+        node(content_tag :div, &Proc.new { filter_type_choice_tag })
+
+        assert_select 'label', 2
+        assert_select 'label', text: I18n.t(:label_all)
+        assert_select 'label', text: I18n.t(:link_filter)
+
+        assert_select 'input[type=radio]', 2
+        assert_select 'input[type=radio][name=type]', 2
+
+        assert_select 'input[type=radio][value=all]', 1
+        assert_select 'input[type=radio]#type-all', 1
+
+        assert_select 'input[type=radio][value=filter]', 1
+        assert_select 'input[type=radio]#type-filter', 1
+      end
+
+      test 'it builds a select field to choose which attributes should be filtered' do
+        node(filter_attribute_choice_tag(['Author', 'Status', 'Created at']))
+
+        assert_select 'div.autocomplete-combobox', 1
+        assert_select 'select#filters-list', 1
+        assert_select 'option', 3
+        assert_select 'option', text: 'Author'
+        assert_select 'option', text: 'Status'
+        assert_select 'option', text: 'Created at'
+      end
+
+      test 'it builds a save filter button when user is allowed to save queries out of query context' do
+        @project = projects(:projects_001)
+        User.any_instance.stubs(:allowed_to?).returns(true)
+
+        node(save_filter_button_tag(true, {filter_content: 'my_filter', user: User.current, project: @project, type: 'Issue'}))
+
+        assert_select 'a', 1
+        assert_select 'a[href=?]', new_project_query_queries_path('rorganize', 'Issue')
+      end
+
+      test 'it builds a save filter button when user is allowed to save queries in a query context' do
+        @project = projects(:projects_001)
+        User.any_instance.stubs(:allowed_to?).returns(true)
+
+        params[:query_id] = 666
+        node(save_filter_button_tag(true, {filter_content: 'my_filter', user: User.current, project: @project, type: 'Issue'}))
+
+        assert_select 'a', 1
+        assert_select 'a#filter-edit-save', 1
+        assert_select 'a[href=?]', edit_query_filter_queries_path(666)
+        assert_select 'a[data-confirm-message=?]', I18n.t(:text_confirm_update_filter)
+      end
+
+      test 'it do not builds a save filter button when user is allowed when filter content is empty' do
+        @project = projects(:projects_001)
+        User.any_instance.stubs(:allowed_to?).returns(true)
+
+        assert_nil save_filter_button_tag(true, {filter_content: '', user: User.current, project: @project, type: 'Issue'})
+      end
+
+      test 'it do not builds a save filter button when user is allowed when filter content is not provided' do
+        @project = projects(:projects_001)
+        User.any_instance.stubs(:allowed_to?).returns(true)
+
+        assert_nil save_filter_button_tag(true, {user: User.current, project: @project, type: 'Issue'})
+      end
+
+      test 'it do not builds a save filter button when user is not allowed to do it' do
+        @project = projects(:projects_001)
+        User.any_instance.stubs(:allowed_to?).returns(false)
+
+        assert_nil save_filter_button_tag(true, {filter_content: 'my_filter', user: User.current, project: @project, type: 'Issue'})
+      end
+
+      test 'it builds a complete filter tag' do
+        filtered_attributes = ['Author', 'Status', 'Created at']
+        submission_path = 'path_to_submission_handler'
+
+        node(filter_tag('issues', filtered_attributes, submission_path, true))
+
+        assert_select 'fieldset#issues-filter', 1
+        assert_select 'legend' do
+          assert_select 'a.toggle', 1
+          assert_select 'a#issues', 1
+        end
+
+        assert_select 'div.content', 1
+        assert_select 'form#filter-form' do
+          assert_select 'table', 1
+          assert_select 'input[type=submit][value=?]', I18n.t(:button_apply)
+        end
+
+      end
+    end
+  end
+end
