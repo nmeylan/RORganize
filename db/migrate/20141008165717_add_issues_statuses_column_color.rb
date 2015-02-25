99831383bd5c51dc84ea4cb6afe041ea942cc24c
nmeylan <nmeylan@gmail.com>
2015-02-25 14:28:00 +0100

    Change default statuses colors.

--- db/migrate/20141008165717_add_issues_statuses_column_color.rb
+++ db/migrate/20141008165717_add_issues_statuses_column_color.rb
@@ -3,24 +3,24 @@ class AddIssuesStatusesColumnColor < ActiveRecord::Migration
     add_column :issues_statuses, :color, :string
 
     issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'New'))
-    issue_status.update_attribute(:color, '#6cc644') unless issue_status.nil?
+    issue_status.update_attribute(:color, '#549e54') unless issue_status.nil?
 
     issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Redo'))
-    issue_status.update_attribute(:color, '#6cc644') unless issue_status.nil?
+    issue_status.update_attribute(:color, '#549e54') unless issue_status.nil?
 
     issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'In progress'))
-    issue_status.update_attribute(:color, '#fbca04') unless issue_status.nil?
+    issue_status.update_attribute(:color, '#ffe166') unless issue_status.nil?
 
     issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Fixed to test'))
-    issue_status.update_attribute(:color, '#fbca04') unless issue_status.nil?
+    issue_status.update_attribute(:color, '#ffe166') unless issue_status.nil?
 
     issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Not satisfying'))
-    issue_status.update_attribute(:color, '#fbca04') unless issue_status.nil?
+    issue_status.update_attribute(:color, '#ffe166') unless issue_status.nil?
 
     issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Closed'))
-    issue_status.update_attribute(:color, '#bd2c00') unless issue_status.nil?
+    issue_status.update_attribute(:color, '#b54223') unless issue_status.nil?
 
     issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Tested to be delivered'))
-    issue_status.update_attribute(:color, '#bd2c00') unless issue_status.nil?
+    issue_status.update_attribute(:color, '#b54223') unless issue_status.nil?
   end
 end
