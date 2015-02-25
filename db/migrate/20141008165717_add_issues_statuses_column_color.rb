af73bb4988ee19d31fa9c36e2341f61dc9eea8a5
nmeylan <nmeylan@gmail.com>
2015-02-25 14:42:56 +0100

    Inverse default color logic for default statuses.

--- db/migrate/20141008165717_add_issues_statuses_column_color.rb
+++ db/migrate/20141008165717_add_issues_statuses_column_color.rb
@@ -3,10 +3,10 @@ class AddIssuesStatusesColumnColor < ActiveRecord::Migration
     add_column :issues_statuses, :color, :string
 
     issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'New'))
-    issue_status.update_attribute(:color, '#549e54') unless issue_status.nil?
+    issue_status.update_attribute(:color, '#b54223') unless issue_status.nil?
 
     issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Redo'))
-    issue_status.update_attribute(:color, '#549e54') unless issue_status.nil?
+    issue_status.update_attribute(:color, '#b54223') unless issue_status.nil?
 
     issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'In progress'))
     issue_status.update_attribute(:color, '#ffe166') unless issue_status.nil?
@@ -18,9 +18,9 @@ class AddIssuesStatusesColumnColor < ActiveRecord::Migration
     issue_status.update_attribute(:color, '#ffe166') unless issue_status.nil?
 
     issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Closed'))
-    issue_status.update_attribute(:color, '#b54223') unless issue_status.nil?
+    issue_status.update_attribute(:color, '#549e54') unless issue_status.nil?
 
     issue_status = IssuesStatus.find_by_enumeration_id(Enumeration.find_by_opt_and_name('ISTS', 'Tested to be delivered'))
-    issue_status.update_attribute(:color, '#b54223') unless issue_status.nil?
+    issue_status.update_attribute(:color, '#549e54') unless issue_status.nil?
   end
 end
