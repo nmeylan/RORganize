adb769cfaa9f869ba4341f84dca9a2af4c7af3e4
nmeylan <nmeylan@gmail.com>
2015-05-02 15:03:24 +0200

    Fix notifier "from"

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -7,7 +7,7 @@ class NotificationMailer < ActionMailer::Base
   def notification_email(notification)
     @notification = notification
     subject = "RORganize - [#{notification.project.slug}] [#{notification.model.class.to_s} ##{notification.model.id}] : #{notification.model.caption}"
-    mail(bcc: notification.recipients.map { |recipient| recipient[:email] }, subject: subject, from: 'RORganize Notifier') do |format|
+    mail(bcc: notification.recipients.map { |recipient| recipient[:email] }, subject: subject) do |format|
       if @notification.trigger.is_a? Journal
         format.html { render file: 'notification_mailer/journal_email' }
         format.text { render file: 'notification_mailer/journal_email' }
@@ -21,7 +21,7 @@ class NotificationMailer < ActionMailer::Base
   def notification_bulk_edit_email(notification)
     @notification = notification
     subject = "RORganize - [#{notification.project.slug}] [#{notification.type} Bulk edit]"
-    mail(bcc: notification.recipients.map { |recipient| recipient[:email] }, subject: subject, from: 'RORganize Notifier') do |format|
+    mail(bcc: notification.recipients.map { |recipient| recipient[:email] }, subject: subject) do |format|
       format.html { render file: 'notification_mailer/bulk_edit_email' }
       format.text { render file: 'notification_mailer/bulk_edit_email' }
     end
@@ -31,12 +31,12 @@ class NotificationMailer < ActionMailer::Base
   def welcome_new_member_email(notification)
     @notification = notification
     subject = "RORganize - [#{notification.project.slug}] : Welcome to the project!"
-    mail(bcc: notification.recipients[0].email, subject: subject, from: 'RORganize Notifier')
+    mail(bcc: notification.recipients[0].email, subject: subject)
   end
 
   def member_join_email(notification)
     @notification = notification
     subject = "RORganize - [#{notification.project.slug}] : A new member joins the project!"
-    mail(bcc: notification.recipients[1].collect { |recipient| recipient.email }, subject: subject, from: 'RORganize Notifier')
+    mail(bcc: notification.recipients[1].collect { |recipient| recipient.email }, subject: subject)
   end
 end
